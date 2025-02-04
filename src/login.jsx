ee8396d195f99f266533858f0bcbf4c41352e77d
PabloTorr03 <pablogdlt03@gmail.com>
2025-02-04 15:59:25 +0100

    Proyecto react creado

---
+++ src/login.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Login = () =>{
+    return(
+        <>
+        <form action="" method="POST">
+            <input type="email" name="email"/>
+            <input type="password" name="password"/>
+            <input type="submit" value=""/>
+        </form>
+        </>
+    )
+}
